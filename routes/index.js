da821cddc705cd668b3829c838557cde8e3d3e25
fengqiang <atguigu@.163.com>
2018-09-19 23:29:21 +0800

    'updata'

--- routes/index.js
+++ routes/index.js
@@ -3,21 +3,14 @@ var router = express.Router();
 const md5 = require('blueimp-md5');
 const {UserModel} = require('../db/models');
 
-const filter = {password: 0, __v: 0}; // 查询时的过滤器(过滤掉password和__v属性)
+const filter = {password: 0, __v: 0};
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.render('index', {title: 'Express'});
 });
 
-
-/*
-测试: 定义注册的路由
-路由回调函编程:
-1. 获取请求参数数据: req.query/param.body
-2. 处理数据: 有可能需要操作数据库
-3. 返回响应: res.send()/json()
- */
+/*测试: 定义注册的路由*/
 /*router.post('/register', function (req, res) {
   // 1. 获取请求参数数据: req.query/param.body
   const {username, password} = req.body
@@ -32,7 +25,7 @@ router.get('/', function (req, res, next) {
 
 })*/
 
-/*定义注册的路由 */
+/*定义注册的路由*/
 router.post('/register', function (req, res) {
   // 1. 获取请求参数数据
   const {username, password, type} = req.body;
@@ -58,7 +51,7 @@ router.post('/register', function (req, res) {
             const _id = userDoc._id;
             // 将用户的id保存到cookie中
             res.cookie('userid', _id);
-            // 返回数据(不能带password)
+            // 返回数据(不带password)
             res.send({
               code: 0,
               data: {
@@ -74,26 +67,44 @@ router.post('/register', function (req, res) {
   })
 });
 
-
 /*定义登陆的路由*/
 router.post('/login', function (req, res) {
   const {username, password} = req.body;
-  // 根据username和password查询users集合
   UserModel.findOne({username, password: md5(password)}, filter, function (error, userDoc) {
-    //如果没有, 说明登陆不能成功, 返回一个失败的响应
+    // 如果没有, 说明登陆不能成功, 返回一个失败的响应
     if (!userDoc) {
       res.send({
         "code": 1,
         "msg": "用户名或密码错误"
       })
     } else {
-      //如果有, 向cookie中保存userid, 返回一个成功的响应
+      // 如果有, 向cookie中保存userid, 返回一个成功的响应
       res.cookie('userid', userDoc._id);
       res.send({
         code: 0,
         data: userDoc
       })
     }
+  })
+});
+
+/*更新用户路由*/
+router.post('/update', function (req, res) {
+  // 得到请求cookie的userid
+  const userid = req.cookies.userid;
+  if (!userid) {// 如果没有, 说明没有登陆, 直接返回提示
+    return res.send({code: 1, msg: '请先登陆'});
+  }
+
+  // 更新数据库中对应的数据
+  UserModel.findByIdAndUpdate({_id: userid}, req.body, function (err, user) {// user是数据库中原来的数据
+    const {_id, username, type} = user;
+    // node端 ...不可用
+    // const data = {...req.body, _id, username, type}
+    // 合并用户信息
+    const data = Object.assign(req.body, {_id, username, type});
+    // assign(obj1, obj2, obj3,...) // 将多个指定的对象进行合并, 返回一个合并后的对象
+    res.send({code: 0, data})
   })
 });
 
